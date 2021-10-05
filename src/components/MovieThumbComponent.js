7cff5faaea03295667253b35643eb3142c808759
Rodrick Coleman <rscole87@gmail.com>
2021-10-05 14:49:10 -0400

    add styling to thumbs

--- src/components/MovieThumbComponent.js
+++ src/components/MovieThumbComponent.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const MovieThumb = ({ movie }) => {
   return (
-    <div className="movie-thumb" style={{backgroundImage: `url(${movie.Poster})`, backgroundPosition: 'center', backgroundSize: 'cover'}}>
-      <h3>{movie.Title} ({movie.Year})</h3>
-      
+    <div className="movie-thumb" style={{ backgroundImage: `url(${movie.Poster})`, backgroundPosition: "center", backgroundSize: "cover" }}>
+      <div className="movie-heading-div">
+        <h3>
+          {movie.Title} ({movie.Year})
+        </h3>
+      </div>
     </div>
   );
 };
